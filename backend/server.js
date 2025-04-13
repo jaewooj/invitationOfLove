a8cedcf462dc8cb9d03d6511531cfa6f1ea748bd
jaewooj <jjw1914@naver.com>
2025-04-13 23:32:32 +0900

    railway DB 연동6

--- backend/server.js
+++ backend/server.js
@@ -20,11 +20,10 @@ app.use(express.json());
 
 // MySQL 연결 풀 설정
 const pool = mysql.createPool({
-  host: process.env.MYSQL_URL,     // MySQL 호스트 (예: mysql.railway.internal)
-  user: process.env.DB_USER,     // MySQL 사용자 (예: root)
-  password: process.env.DB_PASSWORD, // MySQL 비밀번호
-  database: process.env.DB_NAME, // 데이터베이스 이름
-  port: process.env.DB_PORT || 3306, // 포트 (기본값 3306)
+  user: process.env.MYSQLUSER,     // MySQL 사용자 (예: root)
+  password: process.env.MYSQLPASSWORD, // MySQL 비밀번호
+  database: process.env.MYSQL_DATABASE, // 데이터베이스 이름
+  port: process.env.MYSQLPORT || 3306, // 포트 (기본값 3306)
   waitForConnections: true,      // 연결 대기
   connectionLimit: 10,           // 최대 연결 수
   queueLimit: 0,                 // 대기열 제한 없음
